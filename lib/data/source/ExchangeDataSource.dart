d1f0d8de4a67e7d8bba06f700ead03e9df1c29d1
SantinSimone <Santin716@gmail.com>
2024-04-24 22:29:41 +0200

    finire caricamento locale

--- lib/data/source/ExchangeDataSource.dart
+++ lib/data/source/ExchangeDataSource.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
-
-import 'package:eco_swap/data/source/BaseExchangeDataSource.dart';
-import 'package:eco_swap/model/Exchange.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:eco_swap/data/source/BaseExchangeDataSource.dart';
+import 'package:eco_swap/model/Exchange.dart';
 
-class ExchangeDataSource extends BaseExchangeDataSource{
+class ExchangeDataSource extends BaseExchangeDataSource {
+  late Database _database;
   final DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+  bool _isDatabaseInitialized = false;
 
   @override
   Future<String?> loadExchange(Exchange exchange) async {
@@ -17,9 +20,96 @@ class ExchangeDataSource extends BaseExchangeDataSource{
       await _databaseReference.child(databasePath).child(exchange.idToken).set(exchange.toMap());
       return 'Success';
     } catch (error) {
-      print('Errore durante il caricamento del rental: $error');
-      return 'Errore durante il caricamento del rental: $error';
+      print('Errore durante il caricamento dell\'exchange: $error');
+      return 'Errore durante il caricamento dell\'exchange: $error';
     }
+  }
+
+  @override
+  Future<void> loadLocal(Exchange exchange) async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      await _database.insert('exchanges', exchange.toMap());
+    } catch (error) {
+      print('Errore durante il caricamento locale dell\'exchange: $error');
+    }
+  }
+
+  @override
+  Future<List<Exchange>> getLocalExchanges() async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      final List<Map<String, dynamic>> maps = await _database.query('exchanges');
+      return List.generate(maps.length, (i) {
+        return Exchange.fromMap(maps[i]);
+      });
+    } catch (error) {
+      print('Errore durante il recupero degli exchange locali: $error');
+      return [];
+    }
+  }
+
+  @override
+  Future<void> updateLocalExchange(Exchange exchange) async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      await _database.update(
+        'exchanges',
+        exchange.toMap(),
+        where: 'idToken = ?',
+        whereArgs: [exchange.idToken],
+      );
+    } catch (error) {
+      print('Errore durante l\'aggiornamento dell\'exchange locale: $error');
+    }
+  }
+
+  @override
+  Future<void> deleteLocalExchange(String idToken) async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      await _database.delete(
+        'exchanges',
+        where: 'idToken = ?',
+        whereArgs: [idToken],
+      );
+    } catch (error) {
+      print('Errore durante l\'eliminazione dell\'exchange locale: $error');
+    }
+  }
+
+  Future<void> _initializeDatabase() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'exchanges_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE exchanges('
+              'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+              'imagePath TEXT, '
+              'userId TEXT, '
+              'title TEXT, '
+              'description TEXT, '
+              'latitude REAL, '
+              'longitude REAL, '
+              'idToken TEXT, '
+              'imageUrl TEXT)',
+        );
+      },
+      version: 1,
+    );
+    _isDatabaseInitialized = true;
   }
 
   @override
@@ -35,11 +125,5 @@ class ExchangeDataSource extends BaseExchangeDataSource{
       print('Errore durante il caricamento dell\'immagine: $e');
       return '';
     }
-  }
-
-  @override
-  Future<void> loadLocal(Exchange exchange) {
-    // TODO: implement loadLocal
-    throw UnimplementedError();
   }
 }
--- lib/data/source/RentalDataSource.dart
+++ lib/data/source/RentalDataSource.dart
@@ -1,18 +1,24 @@
-import 'dart:io';
-
-import 'package:eco_swap/data/source/BaseRentalDataSource.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+
 import '../../model/Rental.dart';
+import 'BaseRentalDataSource.dart';
 
 class RentalDataSource extends BaseRentalDataSource {
   final DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
-
+  static late Database _database;
+  static bool _isDatabaseInitialized = false;
 
   @override
   Future<String?> loadRental(Rental rental) async {
     try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
       final String databasePath = 'rentals';
       String imageUrl = await uploadImage(rental.imagePath);
       rental.imageUrl = imageUrl;
@@ -26,14 +32,72 @@ class RentalDataSource extends BaseRentalDataSource {
 
   @override
   Future<void> loadLocal(Rental rental) async {
-
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+      await _database.insert('rentals', rental.toMap());
+    } catch (error) {
+      print('Errore durante il caricamento locale del rental: $error');
+    }
   }
 
   @override
+  Future<List<Rental>> getLocalRentals() async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      final List<Map<String, dynamic>> maps = await _database.query('rentals');
+      return List.generate(maps.length, (i) {
+        return Rental.fromMap(maps[i]);
+      });
+    } catch (error) {
+      print('Errore durante il recupero dei rental locali: $error');
+      return [];
+    }
+  }
+
+  @override
+  Future<void> updateLocalRental(Rental rental) async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      await _database.update(
+        'rentals',
+        rental.toMap(),
+        where: 'idToken = ?',
+        whereArgs: [rental.idToken],
+      );
+    } catch (error) {
+      print('Errore durante l\'aggiornamento del rental locale: $error');
+    }
+  }
+
+  @override
+  Future<void> deleteLocalRental(String idToken) async {
+    try {
+      if (!_isDatabaseInitialized) {
+        await _initializeDatabase();
+      }
+
+      await _database.delete(
+        'rentals',
+        where: 'idToken = ?',
+        whereArgs: [idToken],
+      );
+    } catch (error) {
+      print('Errore durante l\'eliminazione del rental locale: $error');
+    }
+  }
+
   Future<String> uploadImage(String imagePath) async {
     try {
       File imageFile = File(imagePath);
-      String fileName = imageFile.path.split('/').last;
+      String fileName = basename(imagePath);
       String filePath = 'rental/$fileName';
       await FirebaseStorage.instance.ref().child(filePath).putFile(imageFile);
       String downloadURL = await FirebaseStorage.instance.ref(filePath).getDownloadURL();
@@ -43,4 +107,30 @@ class RentalDataSource extends BaseRentalDataSource {
       return '';
     }
   }
+
+  Future<void> _initializeDatabase() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'rentals_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE rentals('
+              'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+              'imagePath TEXT, '
+              'userId TEXT, '
+              'title TEXT, '
+              'description TEXT, '
+              'latitude REAL, '
+              'longitude REAL, '
+              'dailyCost TEXT, '
+              'maxDaysRent TEXT, '
+              'idToken TEXT, '
+              'imageUrl TEXT)',
+        );
+      },
+      version: 1,
+    );
+    _isDatabaseInitialized = true;
+  }
+
+// Rimuovi altri metodi per brevità
 }
